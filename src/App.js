c661881d731f40aa15aec2eb026835a37ebd455a
Violeta <violetalopesalves@gmail.com>
2020-05-21 10:26:22 +0100

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -2,7 +2,19 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App">Hello World</div>;
+  return (
+    <div className="App">
+      <div className="container">
+        <h1>React Weather App</h1>
+        <footer>
+          This project is coded by Violeta Pinto Leite and{" "}
+          <a href="https://github.com/violetapintoleite/react-weather-app">
+            open sourced on Git Hub
+          </a>
+        </footer>
+      </div>
+    </div>
+  );
 }
 
 export default App;
