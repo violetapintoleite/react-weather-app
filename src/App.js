25a146d29a09067c2af800eac44d468e19dda4c9
Violeta <violetalopesalves@gmail.com>
2020-05-25 21:47:45 +0100

    Added Linkedin link

--- src/App.js
+++ src/App.js
@@ -8,7 +8,11 @@ function App() {
       <div className="container">
         <Weather defaultCity="Lisbon" />
         <footer>
-          This project is coded by Violeta Pinto Leite and{" "}
+          This project was coded by{" "}
+          <a href="https://www.linkedin.com/in/violetapintoleite/">
+            Violeta Pinto Leite{" "}
+          </a>
+          and{" "}
           <a href="https://github.com/violetapintoleite/react-weather-app">
             open sourced on Git Hub
           </a>
